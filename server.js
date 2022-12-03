68967a2694e07ae38a599402a8589299d2150fa2
Mukesh <mk390270@gmai.com>
2022-12-03 10:07:29 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express=require('express')
+const app=express()
+const path=require('path')
+const ejs=require('ejs')
+const expressLayouts=require('express-ejs-layouts')
+const PORT=process.env.PORT || 3000
+
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+app.use(expressLayouts)
+app.set('views',path.join(__dirname,'/resources/views'))
+app.set('view engine','ejs')
+
+app.listen(PORT,()=>{
+    console.log(`Listening on port ${PORT}`)
+})
