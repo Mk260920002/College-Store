b8595a76160357bd791c3338402022734ec18b09
Mukesh <mk390270@gmai.com>
2022-12-07 13:45:08 +0530

    Made home page

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path=require('path')
 const ejs=require('ejs')
 const expressLayouts=require('express-ejs-layouts')
 const PORT=process.env.PORT || 3000
-
+app.use(express.static('public'))
 app.get('/',(req,res)=>{
     res.render('home')
 })
